d3eff4fa0e314e3a23889af01f2bcaf672c7c19b
Amaljith64 <amaljith64@gmail.com>
2023-01-13 12:57:55 +0530

    fix

--- backend/CodingWithMitchChat/asgi.py
+++ backend/CodingWithMitchChat/asgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-from django.conf.urls import url
+
 from django.urls import path
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CodingWithMitchChat.settings')
 
@@ -21,7 +21,7 @@ from channels.auth import AuthMiddlewareStack
 from chat.consumers import PersonalChatConsumer
 
 
-    
+
 
 application = ProtocolTypeRouter({
     "http" : get_asgi_application(),
