fad2f52b27c04ffa6dde730cba570253cb286c2e
Amaljith64 <amaljith64@gmail.com>
2023-01-13 17:04:41 +0530

    Fixed static root

--- backend/CodingWithMitchChat/settings.py
+++ backend/CodingWithMitchChat/settings.py
@@ -233,14 +233,15 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_LOCATION = config('AWS_LOCATION')
 
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
 
+# STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/build/static/')
 
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 TEMP = os.path.join(BASE_DIR, 'temp')
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
