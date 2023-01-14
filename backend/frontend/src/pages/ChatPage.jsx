5bec93b0149417a29a7f66541449cb8ba042155f
Amaljith64 <amaljith64@gmail.com>
2023-01-14 11:39:01 +0530

    url fix

--- backend/frontend/src/pages/ChatPage.jsx
+++ backend/frontend/src/pages/ChatPage.jsx
@@ -15,7 +15,7 @@ export const ChatPage = () => {
   
 
   const socket = new WebSocket(
-    "ws://127.0.0.1:8000/ws/" + user.user_id + "/" + receiverId + "/"
+    "ws://64.227.186.27/ws/" + user.user_id + "/" + receiverId + "/"
   );
 
   socket.onopen = function (e) {
--- backend/frontend/src/utils/useAxios.js
+++ backend/frontend/src/utils/useAxios.js
@@ -5,7 +5,7 @@ import { useContext } from 'react'
 import AuthContext from '../context/AuthContext'
 
 
-const baseURL = 'http://127.0.0.1:8000'
+const baseURL = 'http://64.227.186.27'
 
 
 const useAxios = () => {
